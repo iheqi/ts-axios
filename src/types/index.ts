262242fbbfb09446e07f2419f339606acbab928e
rubbish-man <993284758@qq.com>
2019-12-08 22:20:30 +0800

    创建入口文件

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Method = 'get' | 'GET' 
+| 'post' | 'POST'
+| 'put' | 'PUT'
+| 'delete' | 'DELETE'
+| 'option' | 'OPTION'
+| 'head' | 'HEAD'
+| 'patch' | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string,
+  method?: Method,
+  data?: any,
+  param?: any
+}
