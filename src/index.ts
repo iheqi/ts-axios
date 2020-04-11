7e2ade16e565ebd26190351ddce8db2f71c4ad67
何琦 <>
2020-04-11 20:48:06 +0800

    post传递 Object

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import { AxiosRequestConfig } from  './types';
-import { buildURL } from './helpers/url';
-import xhr from './xhr';
+import { AxiosRequestConfig } from './types'
+import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
-  processConfig(config);
-  xhr(config);
+  processConfig(config)
+  xhr(config)
 }
 
 function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config);
+  config.url = transformURL(config)
+  config.data = transformData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config;
+  const { url, params } = config
 
-  return buildURL(url, params);
+  return buildURL(url, params)
 }
-export default axios;
+
+function transformData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+export default axios
